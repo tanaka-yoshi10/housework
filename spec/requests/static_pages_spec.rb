1dac63d38fcaa1c6a661044e794ae5ee5faae4d0
Yoshito Tanaka <tanaka.yoshi10@gmail.com>
2014-09-12 01:04:38 +0900

    テストのエラーを対処

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,8 +7,6 @@ describe "Static pages" do
   describe "Home page" do
     before { visit root_path }
 
-    it { should have_content('House Work') }
-    it { should have_title("House Work | Home") }
     it { should have_link('家事一覧', href: house_works_path)}
 
   end
